33b8d8b5669520edc8f50ccf84dd15f7880a7eb9
racindustries <ausorret@gmail.com>
2018-10-28 18:34:10 +0100

    new version

--- js/script.js
+++
@@ -1,9 +0,0 @@
-function unBao() {
-    document.getElementById("hiddenBao").style.display = 'block';
-    document.getElementById("hiddenImage").style.display = 'block';
-    document.getElementById("hiddenImage").scrollIntoView({behavior: "smooth"});
-}
-
-function backToTop() {
-    document.getElementById("header-top").scrollIntoView({behavior: "smooth"});
-}
