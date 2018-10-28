8ef2ed6a05a4e4d37ea256670bfad66111ea9a36
racindustries <ausorret@gmail.com>
2018-10-28 18:25:12 +0100

    Version 1.1
    Blog page added

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@ function unBao() {
     document.getElementById("hiddenImage").style.display = 'block';
     document.getElementById("hiddenImage").scrollIntoView({behavior: "smooth"});
 }
+
+function backToTop() {
+    document.getElementById("header-top").scrollIntoView({behavior: "smooth"});
+}
