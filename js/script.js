a1076129866ee50d96fe020a3194c555e378954d
racindustries <ausorret@gmail.com>
2018-10-28 08:51:51 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function unBao() {
+    document.getElementById("hiddenBao").style.display = 'block';
+    document.getElementById("hiddenImage").style.display = 'block';
+    document.getElementById("hiddenImage").scrollIntoView({behavior: "smooth"});
+}
