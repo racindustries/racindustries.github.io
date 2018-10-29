c5d6bb68ac2307eaf74e3347ce4396afa5ad58bb
racindustries <ausorret@gmail.com>
2018-10-29 13:37:01 +0100

    version clarks

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+function toOrder() {
+    document.getElementById("toCommandChoice").scrollIntoView({behavior: "smooth"});
+}
+
 function unBao() {
     document.getElementById("hiddenBao").style.display = 'block';
     document.getElementById("hiddenImage").style.display = 'block';
@@ -7,3 +11,8 @@ function unBao() {
 function backToTop() {
     document.getElementById("header-top").scrollIntoView({behavior: "smooth"});
 }
+
+function toContact() {
+    document.getElementById("contact-block").scrollIntoView({behavior: "smooth"});
+}
+
