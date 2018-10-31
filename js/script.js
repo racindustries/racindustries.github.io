079bd7ec06109a01543b6f8011f01c53e72e90cb
racindustries <ausorret@gmail.com>
2018-10-31 10:40:17 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
+
+// Add a hamburger menu at the top of the mobile version and hide logo title 'mon bao' when called
 function responsiveMenu() {
     var x = document.getElementById("myTopnav");
     var y = document.getElementById("img-logo");
     if (x.className === "header-topnav") {
         x.className += " responsive";
         y.style.display = "none";
+        $('#mainContainer').css({
+        'padding-top': '100px',
+    });
     } else {
         x.className = "header-topnav"
         y.style.display = "block";
+        $('#mainContainer').css({
+        'padding-top': '0px',
+    });
     }
 }
+
+// NOT WORKING : extend menu width on mobile version
+$(document).ready(function() {
+  $(".header-topnav.responsive").css({
+    'width': ($(".header").width() + 'px')
+  });
+});
+
 
 function toOrder() {
     document.getElementById("toCommandChoice").scrollIntoView({behavior: "smooth"});
@@ -29,19 +45,19 @@ function toContact() {
 }
 
 
-$(function() {
+// $(function() {
 
-  $(window).scroll(function() {
-    var x = $(window).responsiveMenu();
+//   $(window).scroll(function() {
+//     var x = $(window).responsiveMenu();
 
-    if (x > 42) {
-      $(".logo-title").hide();
-    } else {
-      $(".logo-title").show();
-    }
+//     if (x > 42) {
+//       $(".logo-title").hide();
+//     } else {
+//       $(".logo-title").show();
+//     }
 
-  });
+//   });
 
-});
+// });
 
 
